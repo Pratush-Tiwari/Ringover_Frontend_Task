62ac2760619db62309d8570c80dd1568378e5d6b
7357178855 <pratushtiwari011@kgpian.iitkgp.ac.in>
2023-02-05 00:04:46 +0530

    Task Completed

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+import Contact from './components/contact/Contact';
+import Team from './components/team/Team';
+import Nav from "./components/Nav";
+import Journey from "./components/journey/Journey";
+ import Allstore from './components/store/Allstore';
+import Storeshoe from "./components/store/Storeshoe";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Nav></Nav>
+      {/* <Contact></Contact> */}
+      {/* <Team></Team> */}
+      {/* <Journey></Journey> */}
+      {/* <Allstore></Allstore> */}
+      {/* <Storeshoe></Storeshoe> */}
+      {/* <Select></Select> */}
+      <Routes>
+          <Route path="/" element={<Journey></Journey>} />
+        </Routes>
+        <Routes>
+          <Route path="/allproducts" element={<Allstore/>} />
+        </Routes>
+        <Routes>
+          <Route path="/productdetails/:id" element={<Storeshoe/>} />
+        </Routes>
+        <Routes>
+          <Route path="/journey" element={<Journey/>} />
+        </Routes>
+        <Routes>
+          <Route path="/team" element={<Team/>} />
+        </Routes>
+        <Routes>
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
